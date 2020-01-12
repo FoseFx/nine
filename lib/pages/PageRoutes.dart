de58a91bb8f0943cb64fc3a8081d90a423c38824
FoseFx <fosefx@pm.me>
2020-01-12 11:49:51 +0100

    Prototype done, new flutter project created

--- lib/pages/PageRoutes.dart
+++
@@ -1,4 +0,0 @@
-
-const String PAGE_ROUTE_SPLASH    = "/";
-const String PAGE_ROUTE_GAME      = "/game";
-const String PAGE_ROUTE_TUTORIAL  = "/tutorial";
--- lib/prefKeys.dart
+++
@@ -1,2 +0,0 @@
-
-const String PREF_KEY_HAS_COMPLETED_TUTORIAL = "hascompletedtut";
